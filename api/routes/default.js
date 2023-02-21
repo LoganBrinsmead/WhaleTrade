507edf23ce082bdc8137859fd174ad8211fff343
anthony <anthonymolinari>
2023-02-21 09:57:36 -0800

    implemented some of the basic api calls to finnhub for querying stock/symbol info

--- api/routes/default.js
+++ api/routes/default.js
@@ -2,17 +2,20 @@ const express = require('express');
 
 const default_router = express.Router();
 
-
 default_router.get("/", (req, res) => {    
     res.json({
         version: "1.0.0",
     });
+
 });
 
-default_router.post("/posttest", (req, res) => {
+default_router.post("/", (req, res) => {
     let data = req.body;
     console.log(data);
     res.send('ok')
 });
+
+const market_routes = require('./market.router');
+default_router.use("/market", market_routes);
 
 module.exports = default_router;
