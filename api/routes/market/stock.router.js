eaf91085af27ce0610f2a9edd45d3a4d0329408e
anthony <anthonymolinari>
2023-02-22 10:51:47 -0800

    get list of supported mics and exchanges

--- api/routes/market/stock.router.js
+++ api/routes/market/stock.router.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 
 const express = require('express');
 const finnhub_api = require('../../providers/finnhubapi.provider');
+const {exchanges, mics} = require('../../utils/static_market_info');
 
 const apiKey = process.env.FINNHUBAPIKEY;
 
@@ -10,12 +11,12 @@ const stock_router = express.Router({mergeParams: true});
 
 // return a static list of available exchanges supported by finnhub
 stock_router.get('/list/exchanges', (req, res) => {
-    res.sendStatus(200);
+    res.status(200).json(exchanges);
 });
 
 // return a static list of available mics supported by finnhub
 stock_router.get('/list/mics', (req, res) => {
-    res.sendStatus(200);
+    res.status(200).json(mics);
 });
 
 stock_router.get('/exchange=:exchange&mic=:mic', (req, res) => {
