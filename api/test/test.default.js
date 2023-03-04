a738e19fbc7a5350883e8931af11edad3a13acea
anthony <anthonymolinari>
2023-03-04 10:35:55 -0800

    added deps mocha and chia for testing as well as a smoke test for the api.

---
+++ api/test/test.default.js
@@ -0,0 +1,23 @@
+// data api smoke test
+
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+
+chai.use(chaiHttp);
+
+describe('API smoke test', () => {
+    describe('/GET default', () => {
+        it('default api route should return response 200 \'ok\' ', (done) => {
+            chai.request(server)
+                .get('/api/v1')
+                .end((err, res) => {
+                    res.should.have.status(200);
+                    done();
+                });
+        });
+    });
+
+});
