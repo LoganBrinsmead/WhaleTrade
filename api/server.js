f30622c186216b64ff90ae4b819158d4c1f98905
anthony <anthonymolinari>
2023-03-10 09:11:34 -0800

    added cors package

--- api/server.js
+++ api/server.js
@@ -1,12 +1,12 @@
 require('dotenv').config();
 const express = require('express');
-
+const cors = require('cors');
 const PORT = process.env.PORT;
 
 const app = express();
 
 app.use(express.json());
-
+app.use(cors());
 // log incoming requests
 app.use((req, res, next) => {
     console.log(`[${req.method}] - ${req.url}`);
