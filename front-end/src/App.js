659f98515776909827a0aa1a47fea53e85fafb4f
Anthony Molinari <98501823+anthonymolinari@users.noreply.github.com>
2023-03-30 15:54:22 -0700

    deps fixes removed conflict text (#15)
    * add searching bar data
    
    * fixed broken dependencies
    
    * fixed broken dependencies
    
    ---------
    
    Co-authored-by: tyla <gokillboss@yahoo.com>
    Co-authored-by: anthony <anthonymolinari>

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,23 +1,18 @@
 import React from 'react';
-<<<<<<< HEAD
-import Header from './component/Header'
-
-
-=======
+// import Header from './component/Header'
 import CandlestickChart from './charts/CandlestickChart';
->>>>>>> 1c72a58879832adbe722a8bee7027b074b88f58f
+import SearchBar from "./component/SearchBar";
 
 function App () {
   return (
     <div>
-          <Header />
+          {/*<Header />*/}
+        <SearchBar/>
     </div>
   )
 }
 
-<<<<<<< HEAD
-export default App
-=======
+
 // FOR TESTING: visualizing the test chart.
 function chart () {
   return (
@@ -34,5 +29,4 @@ function chart () {
   )
 }
 
-export default chart
->>>>>>> 1c72a58879832adbe722a8bee7027b074b88f58f
+export default App
