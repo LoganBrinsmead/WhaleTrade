1e224cea7a5df62b09175831af18631082a927cc
Ellie Parker <ellie@i-parker.com>
2023-04-07 09:28:02 -0700

    Merged from dev

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,11 +1,5 @@
 import React from 'react';
-<<<<<<< HEAD
-import Header from './component/Header'
-
-
-=======
 import CandlestickChart from './charts/CandlestickChart';
->>>>>>> 1c72a58879832adbe722a8bee7027b074b88f58f
 
 function App () {
   return (
@@ -15,9 +9,6 @@ function App () {
   )
 }
 
-<<<<<<< HEAD
-export default App
-=======
 // FOR TESTING: visualizing the test chart.
 function chart () {
   return (
@@ -35,4 +26,3 @@ function chart () {
 }
 
 export default chart
->>>>>>> 1c72a58879832adbe722a8bee7027b074b88f58f
