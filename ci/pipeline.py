f9d51ef35a59558e1d28d6ac8667163babefe1c7
anthonymolinari <molinaria@sonoma.edu>
2023-04-08 14:14:49 -0700

    pipeline development

--- ci/pipeline.py
+++ ci/pipeline.py
@@ -66,8 +66,8 @@ if __name__ == '__main__':
         if args.login:
             dockerLogin()
         anyio.run(pipeline)
-    except: 
-        print('error')
-    finally:
         if args.login:
             dockerLogout()
+    except: 
+        print('Error Running Pipeline')
+        
