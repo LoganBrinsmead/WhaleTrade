b8fac99362a1e4c5fab03c431de68120ef9b1c90
anthonymolinari <molinaria@sonoma.edu>
2023-04-11 13:35:04 -0700

    pipeline development

--- ci/pipeline.py
+++ ci/pipeline.py
@@ -11,6 +11,63 @@ parser.add_argument("-p", "--push", help="push the image to docker hub", action=
 
 args = parser.parse_args()
 
+async def pipeline_v2():
+    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
+        print('Starting Pipeline v2')
+        
+        src = client.host().directory(".")
+
+        # docker build file
+        dockerfile = src.file("Dockerfile")
+        
+        # run react build script
+        web = (
+            client.container().from_("node:18-alpine")
+                .with_mounted_directory("/app", src)
+                .with_workdir("/app/front-end")
+                .with_exec(["npm", "install"])
+                .with_exec(["npm", "run", "build"])
+        )
+        # await build script
+        await web.exit_code()
+        # save build directory
+        public_src = web.directory("./build")
+        print('react-scripts build finished...Bundling Server...')
+        # bundle api server
+        api = (
+            client.container().from_("node:18-alpine")
+                .with_mounted_directory("/app", src)
+                .with_workdir("/app/api")
+                .with_directory("/app/api/public", public_src)
+                .with_exec(["npm", "install"])
+                .with_exec(["npm", "run", "build"])
+        )
+        # await build script
+        await api.exit_code()
+        print('Bundling Finished...Building Container Image')
+        # save bundle directory
+        dist_src = api.directory("./dist")
+        
+        # # setup build container fs
+        print('Setting up docker build env...')
+        build = (
+            client.container()
+                .with_directory("/build/dist", dist_src)
+                .with_file("/build/Dockerfile", dockerfile)
+                .with_workdir("/build")
+        )
+        # save build dir
+        build_dir = build.directory("/build")
+        # run docker build and push image
+        push = (
+            build
+                .build(build_dir)
+                .publish(f"docker.io/whaletrade/whaletrade:{args.tag}")
+        )
+        await push
+    print('pipeline finished...')
+
+
 async def pipeline():
     async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
         print('Starting Pipeline')
@@ -43,20 +100,6 @@ async def pipeline():
         # save bundled server
         dist_src = api.directory("./dist")
         await dist_src.export("./dist")
-        
-        # build & push image to docker hub
-        # if args.push:
-        #     print('Building & Push Docker Image')
-        #     build = (
-        #         client.container().from_("ubuntu:latest")
-        #             .with_mounted_directory("/app", src)
-        #             .with_workdir("/app")
-        #             .with_mounted_directory("/app/dist", dist_src)
-        #             .build(src)
-        #             .publish(f"docker.io/whaletrade/whaletrade:{args.tag}")
-        #     )
-        #     await build
-        #     print(f'Pushed Image to Dockerhub w/ tag: {args.tag}')
 
     print('Finished Build Pipeline...')
 
@@ -66,7 +109,7 @@ if __name__ == '__main__':
     try:
         if not args.no_login:
             dockerLogin()
-        anyio.run(pipeline)
+        anyio.run(pipeline_v2)
         if not args.no_login:
             dockerLogout()
     except: 
