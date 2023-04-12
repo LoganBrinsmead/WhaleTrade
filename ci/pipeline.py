b4bc39b29ae20adb20934348a21a71a0eb6f261c
anthonymolinari <molinaria@sonoma.edu>
2023-04-11 21:13:47 -0700

    add mongodb config for container build

--- ci/pipeline.py
+++ ci/pipeline.py
@@ -7,7 +7,6 @@ from utils.dockerlogin import dockerLogin, dockerLogout
 parser = argparse.ArgumentParser()
 parser.add_argument("--tag", help="The tag for the container image: ", default="develop")
 parser.add_argument("--no-login", help="do not handle registry login w/ this script", action='store_true')
-parser.add_argument("-p", "--push", help="push the image to docker hub", action='store_true')
 
 args = parser.parse_args()
 
@@ -19,6 +18,8 @@ async def pipeline_v2():
 
         # docker build file
         dockerfile = src.file("Dockerfile")
+        # database config
+        mongodb_config = src.file("./database/mongo_config.yml")
         
         # run react build script
         web = (
@@ -53,6 +54,7 @@ async def pipeline_v2():
         build = (
             client.container()
                 .with_directory("/build/dist", dist_src)
+                .with_file("/build/mongo_config.yml", mongodb_config)
                 .with_file("/build/Dockerfile", dockerfile)
                 .with_workdir("/build")
         )
@@ -66,42 +68,6 @@ async def pipeline_v2():
         )
         await push
     print('pipeline finished...')
-
-
-async def pipeline():
-    async with dagger.Connection(dagger.Config(log_output=sys.stderr)) as client:
-        print('Starting Pipeline')
-        # get project source
-        src = client.host().directory(".")
-        # install deps & run react-scripts build
-        web = (
-            client.container().from_("node:18-alpine")
-                .with_mounted_directory("/app", src)
-                .with_workdir("/app/front-end")
-                .with_exec(["npm", "install"])
-                .with_exec(["npm", "run", "build"])
-        )
-        # await build script for front-end
-        await web.exit_code()
-        # save contents of build dir for later
-        public_src = web.directory("./build")
-        print('react-scripts build finished...Bundling Server...')
-        # bundle backend api
-        api = (
-            client.container().from_("node:18-alpine")
-                .with_mounted_directory("/app", src)
-                .with_workdir("/app/api")
-                .with_directory("/app/api/public", public_src)
-                .with_exec(["npm", "install"])
-                .with_exec(["npm", "run", "build"])
-        )
-        # await build script for backend api
-        await api.exit_code()
-        # save bundled server
-        dist_src = api.directory("./dist")
-        await dist_src.export("./dist")
-
-    print('Finished Build Pipeline...')
 
 
 if __name__ == '__main__':
