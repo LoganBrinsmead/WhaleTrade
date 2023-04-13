096b97b17db6e496c50276180d8548027cb9a423
Ellie Parker <ellie@i-parker.com>
2023-04-12 20:23:12 -0700

    Added stock summary component and made progress on font formating

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import CandlestickChart from './charts/CandlestickChart';
+import './App.css';
 
 function App () {
   return (
-    <div>
+    <div className="font-face-rht">
           <Header />
     </div>
   )
--- front-end/src/index.js
+++ front-end/src/index.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './fonts/red_hat_display/RedHatDisplay-Bold.ttf'
+import './fonts/red_hat_text/RedHatText-Regular.tff'
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
